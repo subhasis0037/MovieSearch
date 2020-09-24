3b597d12045bd86684b773d2941490f9a51d2ce3
subhasis0037 <60254917+subhasis0037@users.noreply.github.com>
2020-09-24 02:16:15 -0700

    Add files via upload

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React ,{useState} from "react";
 
 import "./MovieRow.css";
 
-const MovieRow = (movie) => {
+const MovieRow = (props) => {
+  const [movieLink,setMovieLink]= useState("");
+  const src = props.movie.id;
+  console.log(src);
+
+  const viewDetails = () => {
+  setMovieLink(src);
+
+  console.log("view movie details !" + movieLink );
+  window.location.href=("https://www.themoviedb.org/movie/"+ movieLink )
+
+  }
+
   return (
-    <table key={movie.id}>
+    <div>
+    <table key={props.movie.id}>
       <tbody>
         <tr>
           <td>
-            <img src={movie.poster_src} className="moviePic" alt="poster"></img>
+            <img src={props.poster_src} className="moviePic" alt="poster"></img>
           </td>
           <td className="title">
-            <h2>{movie.title}</h2>
-            <p>{movie.overview}</p>
+            <h4>{props.movie.title}</h4>
+            <p>{props.movie.overview}</p>
           </td>
+        </tr>
+        <tr>
+        <td>
+        <button onClick={viewDetails} className= "view" type="button">View</button>
+        </td>
+        <td>
+        <button className= "wishlist" type="button">+ WishList</button>
+        </td>
         </tr>
       </tbody>
     </table>
+
+    </div>
   );
 };
 export default MovieRow;
